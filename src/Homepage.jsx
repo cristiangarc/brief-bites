47adbf3aa42ae2fd9a9fb9221d774a230c2d44fb
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-09 15:49:23 -0400

    added a background color to Homepage, and imported the homepage css file

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,8 +1,9 @@
 import Sections from "./components/Sections";
+import './Homepage.css';
 
 function Homepage ({ sections }) {
     return (
-        <div>
+        <div className="homepage">
             <h1>NYT RSS Feed API Sections & Articles</h1>
             <Sections sections={sections} />
         </div>
