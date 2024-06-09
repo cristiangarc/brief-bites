79b8c677a344a767ff523bbdeeeca0a66cc3597d
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-09 17:16:01 -0400

    resolved merge conflicts

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,50 +1,79 @@
 import { useState, useEffect, createContext } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { getNYTArticles } from "./fetch";
-import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuContent, NavigationMenuLink, NavigationMenuTrigger } from "@radix-ui/react-navigation-menu";
-import './Section.css';
-import main from './OpenAi';
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+} from "@radix-ui/react-navigation-menu";
+import "./Section.css";
+import main from "./OpenAi";
 
 export const SummaryContext = createContext(null);
 
 const Section = ({ sections }) => {
-    const [articles, setArticles] = useState([]);
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getNYTArticles(sections[id]).then((results) => {
-            setArticles(results);
-        }).catch((err) => console.error(err));
-    }, []);
+  const [summary, setSummary] = useState("");
+  const [articles, setArticles] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    getNYTArticles(sections[id])
+      .then((results) => {
+        setArticles(results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-    const handleClick = (art) => {
-        main()
-        .then(response => {
-            navigate('/sections/summary', { state: {
-                article: art,
-                summary: response,
-            } } );
-        })
-     }
+  const handleClick = async (articles) => {
+    const response = await main(articles)
+     setSummary(response)
+  };
 
-    return (
-         <SummaryContext.Provider value={{}}>
-        <div>
-            <h1>{sections[id]}</h1>
-            {articles.length > 0 && articles.map(article =>
-                <li key={article.slug_name}>
-                    <Link to={article.url} target="_blank">{article.title}</Link>
-                    <p>{article.abstract}</p>
-                    <button onClick={()=> handleClick(article)}>Summarize (8th Grade)</button>
-                    <button>Summarize (High School)</button>
-                    <button>Summarize (Adults)</button>
-                    <br />
-                    <br />
-                </li>)}
-        </div>
-         </SummaryContext.Provider> 
-    )
-}
+  console.log(summary)
 
-export default Section; 
+  return (
+    <SummaryContext.Provider
+      value={{ summary, setSummary, articles, setArticles }}
+    > 
+    {
+            (summary) ? <div>{summary}</div> : (
+                <div>
+                  <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink>
+                          <Link className="home-link" to={"/"}>
+                            Home
+                          </Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                  <h1>{sections[id]}</h1>
+                  {articles.length > 0 &&
+                    articles.map((article) => (
+                      <li key={article.slug_name}>
+                        <Link to={article.url} target="_blank">
+                          {article.title}
+                        </Link>
+                        <p>{article.abstract}</p>
+                        <button onClick={() => handleClick(article)}>
+                          Summarize for 8th Graders + Below
+                        </button>
+                        <button>Summarize for High Schoolers</button>
+                        <button>Summarize for Adults</button>
+                      </li>
+                    ))}
+                </div>
+                )  
+    }
+
+    </SummaryContext.Provider>
+  );
+};
+
+export default Section;
