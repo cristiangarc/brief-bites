3873f853747546244bd3fa24edf31bd7b6d10190
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-14 08:23:10 -0400

    import the NYT API KEY as an env variable

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,7 +1,6 @@
-// const API_KEY = import.meta.env.VITE_API_KEY;
-const newApiKey = 'SaLgMxPpyGLGmk7XmEs2GjqaBKVudsd2'
+const NYT_API_KEY = import.meta.env.VITE_NYT_API_KEY;
 export async function getNYTArticles (section) {
-    const response = await fetch(`https://api.nytimes.com/svc/news/v3/content/all/${section}.json?api-key=${newApiKey}`);
+    const response = await fetch(`https://api.nytimes.com/svc/news/v3/content/all/${section}.json?api-key=${NYT_API_KEY}`);
     const data = await response.json()
     return data.results;
 }
