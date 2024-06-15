aef1adae33b2ca1f0238c48b56fd3bf89123b80a
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-15 12:50:13 -0400

    import the NYT api key as a environment variable

--- src/api/nytimes-api.js
+++ src/api/nytimes-api.js
@@ -1,9 +1,11 @@
-// const API_KEY = import.meta.env.VITE_API_KEY;
-import { XMLParser } from "fast-xml-parser";
-
+const NYT_API_KEY = import.meta.env.VITE_NYT_API_KEY;
 export async function getNYTArticles (section) {
-    return fetch(`https://rss.nytimes.com/services/xml/rss/nyt/${section}.xml`)
-        .then(response => response.text())
-        .then(text => new XMLParser().parse(text))
-       .then(data => data.rss.channel.item);
+    try {
+        const response = await fetch(`https://api.nytimes.com/svc/news/v3/content/all/${section}.json?api-key=${NYT_API_KEY}`);
+        const data = await response.json()
+        return data.results;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 }
