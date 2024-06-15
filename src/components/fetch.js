df5ad04fd03bee13c231670e64479f1addd81896
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-15 10:58:20 -0400

    deleted an extra backend api fetch file

--- src/components/fetch.js
+++
@@ -1,6 +0,0 @@
-const NYT_API_KEY = import.meta.env.VITE_NYT_API_KEY;
-export async function getNYTArticles (section) {
-    const response = await fetch(`https://api.nytimes.com/svc/news/v3/content/all/${section}.json?api-key=${NYT_API_KEY}`);
-    const data = await response.json()
-    return data.results;
-}
